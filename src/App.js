0dd044808228b8e91a3f8b0f51651f68af8cb32b
Ramal <ramal.salha@gmail.com>
2019-07-14 01:27:44 +0200

    Added Workout component for MyWorkout view.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import Main from "./views/Main";
+import MyWorkouts from "./views/MyWorkouts";
 
 import "./App.css";
 
@@ -9,6 +10,7 @@ function App() {
       <div className="App">
          <Router>
             <Route component={Main} exact path="/" />
+            <Route component={MyWorkouts} path="/myworkouts" />
          </Router>
       </div>
    );
--- src/views/MyWorkouts/index.jsx
+++ src/views/MyWorkouts/index.jsx
@@ -1,14 +1,27 @@
-import React, { Component } from 'react';   
+import React, { Component } from "react";
+import Workout from "./components/Workout";
+
+import "./MyWorkouts.css";
 
 class MyWorkouts extends Component {
-    state = {  }
-    render() { 
-        return ( 
-            <React.Fragment>
-                
-            </React.Fragment>
-         );
-    }
+   state = {
+      workouts: [{ name: "workout1" }, { name: "workout2" }]
+   };
+
+   render() {
+      const { workouts } = this.state;
+      return (
+         <React.Fragment>
+            <div className="my-workouts">
+               <div>
+                  {workouts.map((data, i) => (
+                     <Workout key={i} data={data} />
+                  ))}
+               </div>
+            </div>
+         </React.Fragment>
+      );
+   }
 }
- 
+
 export default MyWorkouts;
